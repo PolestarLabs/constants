ccd61aceaec09c560f6380fc6433e45591bd18aa
Tsumari <lucasflicky@hotmail.com>
2021-07-05 10:53:08 -0300

    revert

---
+++ ui-colors.ts
@@ -0,0 +1,52 @@
+
+export enum RarityColors {
+  C=  "#928fa8",
+  U=  "#63b361",
+  R=  "#3646bf",
+  SR=  "#8827ce",
+  UR=  "#dc5c50",
+  XR=  "#981f1f",
+}
+
+export enum Branding {
+  pink=     "#FF5599",
+  grape=    "#AA66FF",
+  purple=   "#AA66FF",
+  purple2=  "#5E38BD",
+  lilac=    "#CC55FF",
+  yellow=   "#F8B95F",
+  red=      "#FC5065",
+  blue=     "#5A90F5",
+  cyan=     "#50CCFF",
+  white=    "#EDEDFF",
+  green=    "#2BE0AF",
+  green2=   "#2BC080",
+  jade=     "#2BA0AF",
+  softblack="#3A3A55",
+  black=    "#1B1B2B",  
+  dark=     "#2B2B3F",
+  
+}
+
+export enum Misc {
+  salmon=  "#FF8580",
+  orange=  "#FF8045",
+  lime=    "#90F075",
+}
+
+export enum Interface {
+  red=      ("#FC3050"),
+  green=    Branding.green2,
+  yellow=   Branding.yellow,
+  blue=     Branding.blue,
+  //aliases
+  danger=   red,
+  error=    red,
+  warning=  yellow,
+  success=  green,
+  info=     blue,
+  
+  primary=    Branding.pink,
+  secondary=  Branding.purple,
+  
+} 
