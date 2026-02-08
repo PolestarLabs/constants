f3c60eb5b07e706d6ad52ff64ca9f838e17fea5f
Flicky <flicky@pollux.gg>
2026-02-08 20:33:34 -0300

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  Clusters: require("./clusters").default,
+  countries: require("./countries").default,
+  discoin: require("./discoin").default,
+  lootbox: require("./lootbox"),
+  shop: require("./shop"),
+  timers: require("./timers"),
+  uiColors: require("./ui-colors"),
+  league: require("./league.json"),
+};
---
+++ index.ts
@@ -0,0 +1,8 @@
+export { default as Clusters } from "./clusters";
+export { default as countries } from "./countries";
+export { default as discoin } from "./discoin";
+export * from "./lootbox";
+export * from "./shop";
+export * from "./timers";
+export * from "./ui-colors";
+export { default as league } from "./league.json";
